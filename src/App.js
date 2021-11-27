3aedb5f98a7ff73aa204a7fdd3c4173469dbab8b
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2021-11-27 14:08:45 -0300

    By

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import logo from './logo.svg';
-import html2canvas from 'html2canvas';
-import { useState } from 'react';
-import './App.css';
-
+import logo from "./logo.svg";
+import html2canvas from "html2canvas";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [linea1, setLinea1] = useState('');
-  const [linea2, setLinea2] = useState('');
-  const [imagen, setImagen] = useState('');
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [imagen, setImagen] = useState("");
 
   const onChangeLinea1 = function (evento) {
-    setLinea1(evento.target.value)
-  }
+    setLinea1(evento.target.value);
+  };
 
   const onChangeLinea2 = function (evento) {
-    setLinea2(evento.target.value)
-  }
+    setLinea2(evento.target.value);
+  };
 
   const onChangeImagen = function (evento) {
-    setImagen(evento.target.value)
-  }
+    setImagen(evento.target.value);
+  };
 
   const onClickExportar = function (evento) {
-
-    html2canvas(document.querySelector("#meme")).then(canvas => {
+    html2canvas(document.querySelector("#meme")).then((canvas) => {
       var img = canvas.toDataURL("image/png");
-      var link = document.createElement('a');
-      link.download = 'meme.png';
+      var link = document.createElement("a");
+      link.download = "meme.png";
       link.href = img;
       link.click();
     });
-  }
+  };
   return (
-    <div className="App"><br/>
-    <span>Dr. MEME Generator </span>
-     <br/>
-     <br/>
+    <div className="App">
+      <br />
+      <span>Dr. MEME Generator By. DR.</span>
+      <br />
+      <br />
       <select onChange={onChangeImagen}>
         <option value="fire">Casa en llamas</option>
         <option value="futurama">Futurama</option>
@@ -44,18 +42,18 @@ function App() {
         <option value="matrix">Matrix</option>
         <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart</option>
-      </select> <br/>
-
-      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" /> <br />
-      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" /> <br />
+      </select>{" "}
+      <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" />{" "}
+      <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />{" "}
+      <br />
       <button onClick={onClickExportar}>Exportar</button>
-
       <div className="meme" id="meme">
         <span>{linea2}</span> <br />
         <span>{linea1}</span>
         <img src={"/img/memes/" + imagen + ".jpg"} />
       </div>
-
     </div>
   );
 }
